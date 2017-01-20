e443f35f26a000228ed8ec2f6ec1bd9ef91b181f
todorjelev36@gmail.com <todorjelev36@gmail.com>
2017-01-20 22:06:23 +0200

    + adding gun script

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -24,23 +24,8 @@ public class Gun : MonoBehaviour
 		// If the fire button is pressed...
 		if(Input.GetButtonDown("Fire1"))
 		{
-			// ... set the animator Shoot trigger parameter and play the audioclip.
-			anim.SetTrigger("Shoot");
-			GetComponent<AudioSource>().Play();
-
-			// If the player is facing right...
-			if(playerCtrl.facingRight)
-			{
-				// ... instantiate the rocket facing right and set it's velocity to the right. 
-				Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-				bulletInstance.velocity = new Vector2(speed, 0);
-			}
-			else
-			{
-				// Otherwise instantiate the rocket facing left and set it's velocity to the left.
-				Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,180f))) as Rigidbody2D;
-				bulletInstance.velocity = new Vector2(-speed, 0);
-			}
+            Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
+            bulletInstance.velocity = new Vector2(speed, 0);
 		}
 	}
 }
