ef9e5c54708b9d6e5240f171996a0a4f8c781ae7
Ivan Nikolov <inikolov90@gmail.com>
2017-01-20 22:10:48 +0200

    added jump to movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,33 +6,52 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Movement : MonoBehaviour {
 		
-	float speed = 10.0f;
+	public float speed = 10.0f;
+	public float jumpForce = 10.0f;
+
+	private Transform groundCheck;
+	private bool grounded = false;
+	private bool jump = false;
 	
 	// Update is called once per frame
 
+	void Start() {
+		groundCheck = transform.FindChild ("groundCheck");
+	}
 
+	void Update() {
+		
+		grounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
 
-	void Update () {
+		Debug.Log (grounded);
+		// If the jump button is pressed and the player is grounded then the player should jump.
+		if(Input.GetButtonDown("Jump") && grounded)
+			jump = true;
+	}
+
+	void FixedUpdate () {
 		
 		float x = Input.GetAxisRaw ("Horizontal");
 
-		Vector3 direction = new Vector3 (0.0f, 0.0f ,x); /// depends on the main camera movement
+		Vector3 direction = new Vector3 (0.0f, 0.0f ,-x); /// depends on the main camera movement
 		direction.Normalize();
 		transform.position += Time.deltaTime * speed * direction;
-		if (Input.GetAxis ("Jump") != 0.0f) {
+		if (Input.GetAxis ("Jump") != 0.0f && jump) {
 			Jump ();
+
 		}
 	}
 
 	void Jump() {
 
+		jump = false;
 
 		Rigidbody rigidbody = transform.GetComponent<Rigidbody> ();
 		Debug.AssertFormat (rigidbody != null, "Movement script for {0} needs rigitbody", transform.name);
 
 		if (rigidbody != null) {
 			
-			rigidbody.AddForce (Vector3.up, ForceMode.Impulse);
+			rigidbody.AddForce (Vector3.up * jumpForce, ForceMode.Impulse);
 		}
 	}
 }
