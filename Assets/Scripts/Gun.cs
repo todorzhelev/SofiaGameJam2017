3578d6e91f122a94c3be4041021eca77d2edcf54
todorjelev36@gmail.com <todorjelev36@gmail.com>
2017-01-21 22:12:51 +0200

    - deleting particles

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -96,8 +96,8 @@ public class Gun : MonoBehaviour
 
             if (particlesList[i].m_lifetime <= 0)
             {
-                //Destroy(particlesList[i].m_rigidBody.gameObject);
-//                particlesList.RemoveAt(i);
+                Destroy(particlesList[i].m_rigidBody.gameObject);
+                particlesList.RemoveAt(i);
             }
         }
 	}
