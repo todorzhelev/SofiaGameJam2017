64f4a0c60f65b0f5a2827099d276dbf02a599f9e
Ivan Nikolov <inikolov90@gmail.com>
2017-01-21 12:45:36 +0200

    joysticks

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -17,14 +17,17 @@ public class Movement : MonoBehaviour {
 
 	public PlayerPrefix prefix;
 
-	private Transform groundCheck;
 	private bool grounded = false;
 	private bool jump = false;
 	
 	// Update is called once per frame
 
+	private string horizontalAxis;
+	private string jumpAxis;
+
 	void Start() {
-		groundCheck = transform.FindChild ("groundCheck");
+		horizontalAxis = prefix.ToString () + "Horizontal";
+		jumpAxis = prefix.ToString() + "Jump";
 	}
 
 	void Update() {
@@ -33,18 +36,18 @@ public class Movement : MonoBehaviour {
 
 		Debug.Log (grounded);
 		// If the jump button is pressed and the player is grounded then the player should jump.
-		if(Input.GetButtonDown("Jump") && grounded)
+		if(Input.GetButtonDown(jumpAxis) && grounded)
 			jump = true;
 	}
 
 	void FixedUpdate () {
 		
-		float x = Input.GetAxisRaw (prefix.ToString() + "Horizontal");
+		float x = Input.GetAxisRaw (horizontalAxis);
 
-		Vector3 direction = new Vector3 (0.0f, 0.0f ,-x); /// depends on the main camera movement
+		Vector3 direction = new Vector3 (-x, 0.0f , 0.0f); /// depends on the main camera movement
 		direction.Normalize();
 		transform.position += Time.deltaTime * speed * direction;
-		if (Input.GetAxis (prefix.ToString() + "Jump") != 0.0f && jump) {
+		if (Input.GetAxis (jumpAxis) != 0.0f && jump) {
 			Jump ();
 
 		}
