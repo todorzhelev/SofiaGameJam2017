0555380a7390bd797906dd06555e13e69df65865
Ivan Nikolov <inikolov90@gmail.com>
2017-01-21 23:50:50 +0200

    rename Collision to Wall collision

--- Assets/Scripts/WallCollision.cs
+++ Assets/Scripts/WallCollision.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Collision : MonoBehaviour {
+public class WallCollision : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
