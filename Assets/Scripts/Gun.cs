3c610f881d9d702c0f09c33ba231578086e58998
todorjelev36@gmail.com <todorjelev36@gmail.com>
2017-01-21 11:22:06 +0200

    + adding particles list + particles are dying after some time

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Gun : MonoBehaviour
 {
@@ -12,21 +13,63 @@ public class Gun : MonoBehaviour
     public float waveFrequency;
     public float waveLength;
 
-	void Awake()
+    public List<Particle> particlesList;
+
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////////////
+    /// </summary>
+    public struct Particle
+    {
+        public Particle(Rigidbody body, int Lifetime)
+        {
+            rigidBody = body;
+            lifetime = Lifetime;
+        }
+
+        public Rigidbody rigidBody;
+        public int lifetime;
+    }
+
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////////////
+    /// </summary>
+    /// 
+    void Awake()
 	{
-		// Setting up the references.
-		anim = transform.root.gameObject.GetComponent<Animator>();
+        particlesList = new List<Particle>();
+        // Setting up the references.
+        anim = transform.root.gameObject.GetComponent<Animator>();
 		playerCtrl = transform.root.GetComponent<PlayerControl>();
 	}
 
-
-	void Update ()
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////////////
+    /// </summary>
+    void Update ()
 	{
 		if(Input.GetButtonDown("Fire1"))
 		{
             Rigidbody bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody;
-            
+
+            Particle particle = new Particle();
+            particle.rigidBody = bulletInstance;
+            particle.lifetime = 100;
+
             bulletInstance.AddForce(transform.forward * speed);
+
+            particlesList.Add(particle);
+        }
+
+        for (int i = 0; i < particlesList.Count; i++)
+        {
+            Particle particle = particlesList[i];
+            particlesList[i] = new Particle(particle.rigidBody, --particle.lifetime);
+
+            if (particlesList[i].lifetime <= 0)
+            {
+                Destroy(particlesList[i].rigidBody.gameObject);
+                particlesList.RemoveAt(i);
+            }
         }
 	}
 }
