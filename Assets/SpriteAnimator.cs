a54c17b741e3764c4922bd27889c949e5d45c48c
Ivan Nikolov <inikolov90@gmail.com>
2017-01-21 18:10:54 +0200

    [WIP] Animation controller

---
+++ Assets/SpriteAnimator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SpriteAnimator : MonoBehaviour {
+
+
+	public string idle = "idle";
+	public string shoot = "Shoot";
+	public string Run = "Run";
+	public string RunAndShoot = "Run Shoot";
+
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+		anim = transform.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			anim.Play ("Run");
+
+		}
+	}
+}
