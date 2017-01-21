b8ab691bcef5c3e7c828b63b0fdf473a2af43519
Ivan Nikolov <inikolov90@gmail.com>
2017-01-21 11:41:53 +0200

    switched player controls to joysticks

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public enum PlayerPrefix
+{
+	P1_,
+	P2_
+}
+
 [RequireComponent(typeof(Rigidbody))]
 public class Movement : MonoBehaviour {
 		
 	public float speed = 10.0f;
 	public float jumpForce = 10.0f;
+
+	public PlayerPrefix prefix;
 
 	private Transform groundCheck;
 	private bool grounded = false;
@@ -31,12 +39,12 @@ public class Movement : MonoBehaviour {
 
 	void FixedUpdate () {
 		
-		float x = Input.GetAxisRaw ("Horizontal");
+		float x = Input.GetAxisRaw (prefix.ToString() + "Horizontal");
 
 		Vector3 direction = new Vector3 (0.0f, 0.0f ,-x); /// depends on the main camera movement
 		direction.Normalize();
 		transform.position += Time.deltaTime * speed * direction;
-		if (Input.GetAxis ("Jump") != 0.0f && jump) {
+		if (Input.GetAxis (prefix.ToString() + "Jump") != 0.0f && jump) {
 			Jump ();
 
 		}
