07bcfaeef23d23fc41f587eb3867ce5e333b8fd4
Ivan Nikolov <inikolov90@gmail.com>
2017-01-21 22:33:24 +0200

    Add door prefab

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+	public bool closed = true;
+
+	public float smooth = 1f;
+	private Vector3 targetAngles;
+
+	private Transform door;
+	private Transform wall;
+	void Start() {
+		door = transform.FindChild ("door");
+		wall = transform.FindChild ("wall");
+		targetAngles = transform.eulerAngles + 120f * Vector3.up; 
+	}
+
+	public void Open() {
+		closed = false;
+		wall.gameObject.SetActive (false);
+		StartCoroutine ("ExecOpen");
+	}
+
+
+	IEnumerator ExecOpen()
+	{
+		while(true)
+		{
+			if (door.eulerAngles.z >= 130.0f) {
+				StopAllCoroutines ();
+			}
+			door.eulerAngles = Vector3.Lerp (door.eulerAngles, targetAngles, 10 * smooth * Time.deltaTime);
+
+			yield return new WaitForFixedUpdate();
+		}
+	}
+}
---
+++ Assets/DoorTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour {
+
+
+	public DoorScript door;
+
+	// Use this for initialization
+	void Start () {
+		door = transform.GetComponentInParent<DoorScript> ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.transform.CompareTag ("Player") && door.closed) {
+			door.Open ();
+		}
+	}
+}
