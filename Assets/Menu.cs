60045a069814f14fdabfa4454d02c12b41d5c6f1
Ivan Nikolov <inikolov90@gmail.com>
2017-01-22 06:59:45 +0200

    add main menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public Transform menuPanel;
+	public Transform creditsPanel;
+
+	public void StartGame() {
+		SceneManager.LoadScene ("level01",LoadSceneMode.Single);
+	}
+
+	public void ShowCredits() {
+		menuPanel.gameObject.SetActive (false);
+		creditsPanel.gameObject.SetActive (true);
+	}
+
+	public void HideCredits () {
+		menuPanel.gameObject.SetActive (true);
+		creditsPanel.gameObject.SetActive (false);
+	}
+
+	public void Exit() {
+
+	}
+}
