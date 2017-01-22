4649eff18518ff0384aa3cea087d25c6dae42aa0
Ivan Nikolov <inikolov90@gmail.com>
2017-01-22 15:23:28 +0200

    add PlayerBlocker

---
+++ Assets/PlayerBlocker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlocker : MonoBehaviour {
+
+
+	public Transform tpPos;
+
+	void OnTriggerEnter(Collider other) {
+		print ("enter");
+		if (other.transform.CompareTag ("Player")) {
+			other.transform.position = tpPos.position;
+		}
+	}
+	void OnTriggerExit(Collider other) {
+
+	}
+}
